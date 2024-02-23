ab47f21c411f8c9c8a07dab19b916b7181039539
Lcdiego <diegocolman78@hotmail.com>
2024-02-23 18:51:42 -0300

    bg buttons nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -34,34 +34,34 @@ const Navbar= () =>{
 			</button>
 		</div>
 		<ul className={`hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6 ${isMenuOpen ? '' : 'hidden'}`}>
-			<li><a className="text-sm text-gray-400 hover:text-gray-500" href="#">Home</a></li>
+			<li><a className="text-sm text-gray-400 hover:text-blue-600 font-bold" href="#">Home</a></li>
 			<li className="text-gray-300">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
 				</svg>
 			</li>
-			<li><a className="text-sm text-gray-400 font-bold" href="#">About Us</a></li>
+			<li><a className="text-sm text-gray-400 hover:text-blue-600 font-bold" href="#">About Us</a></li>
 			<li className="text-gray-300">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
 				</svg>
 			</li>
-			<li><a className="text-sm text-gray-400 hover:text-gray-500" href="#">Services</a></li>
+			<li><a className="text-sm text-gray-400 hover:text-blue-600 font-bold" href="#">Services</a></li>
 			<li className="text-gray-300">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
 				</svg>
 			</li>
-			<li><a className="text-sm text-gray-400 hover:text-gray-500" href="#">Pricing</a></li>
+			<li><a className="text-sm text-gray-400 hover:text-blue-600 font-bold" href="#">Pricing</a></li>
 			<li className="text-gray-300">
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
 					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
 				</svg>
 			</li>
-			<li><a className="text-sm text-gray-400 hover:text-gray-500" href="#">Contact</a></li>
+			<li><a className="text-sm text-gray-400 hover:text-blue-600 font-bold" href="#">Contact</a></li>
 		</ul>
-		<a className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" href="#">Sign In</a>
-		<a className="hidden lg:inline-block py-2 px-6 bg-white hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" href="#">Sign up</a>
+		<a className="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-blue-600 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" href="#">Sign In</a>
+		<a className="hidden lg:inline-block py-2 px-6 bg-white hover:bg-blue-600 text-sm text-gray-900 font-bold rounded-xl transition duration-200" href="#">Sign up</a>
 	</nav>
   <div className={`navbar-menu relative z-50 ${isMenuOpen ? '' : 'hidden'}`}>
   <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25" onClick={handleCloseMenu}></div>
@@ -99,8 +99,8 @@ const Navbar= () =>{
 			</div>
 			<div className="mt-auto">
 				<div className="pt-6">
-					<a className="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold leading-none bg-gray-50 hover:bg-gray-100 rounded-xl" href="#">Sign in</a>
-					<a className="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-blue-600 hover:bg-blue-700  rounded-xl" href="#">Sign Up</a>
+					<a className="block px-4 py-3 mb-3 leading-loose text-xs text-center text-white font-semibold bg-gray-500 hover:bg-blue-700 rounded-xl" href="#">Sign in</a>
+					<a className="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-gray-500 hover:bg-blue-700  rounded-xl" href="#">Sign Up</a>
 				</div>
 				<p className="my-4 text-xs text-center text-gray-400">
 					<span>Copyright © 2021</span>
